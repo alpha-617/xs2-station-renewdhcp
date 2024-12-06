ad22a30afe3751577ffed795e90cc40e33b2801f
alpha-617 <55421952+alpha-617@users.noreply.github.com>
2024-12-06 11:49:16 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-wanIp="192.168.1.1"
-COUNT=4
+# Configuration
+WAN_IP="192.168.1.1"
+PING_COUNT=4
+USERNAME="ubnt"
+HOST="192.168.2.254"
+PASSWORD="supardi" # Plain text password; consider using more secure methods for sensitive data.
+SCRIPT1="/usr/etc/rc.d/rc start"
+SCRIPT2="/sbin/udhcpc -i ath0 -s /etc/udhcpc/udhcpc -p /var/run/udhcpc.ath0.pid"
+LOG_FILE="/var/log/ping_failure.log"
 
-# Ping the IP address
-ping -c $COUNT $wanIp > /dev/null 2>&1
+# Function to execute a remote command via SSH
+execute_ssh_command() {
+    local command=$1
+    sshpass -p "${PASSWORD}" ssh "${USERNAME}@${HOST}" "${command}"
+}
 
-if [ $? -ne 0 ]; then
-    echo "Ping to $wanIp failed. Executing the command..."
-	USERNAME=ubnt
-	#ip address
-	HOST="192.168.2.254"
-	#the password without quote
-	PASS=supardi
-	SCRIPT1="/usr/etc/rc.d/rc start"
-	SCRIPT2="/sbin/udhcpc -i ath0 -s /etc/udhcpc/udhcpc -p /var/run/udhcpc.ath0.pid"
+# Check connectivity by pinging the WAN IP
+if ! ping -c "${PING_COUNT}" "${WAN_IP}" > /dev/null 2>&1; then
+    echo "Ping to ${WAN_IP} failed. Executing recovery commands..."
+    echo -e "Restarting... This may take a minute."
 
-	echo -e "restarting... it may take a minute"
-sshpass -p ${PASS} ssh  ${USERNAME}@${HOST} "${SCRIPT1}"
-	sleep 5
-sshpass -p ${PASS} ssh  ${USERNAME}@${HOST} "${SCRIPT2}" 
-	echo -e "adding route....."
-   echo "$(date): Ping to $wanIp failed" >> /var/log/ping_failure.log
+    # Execute remote commands
+    execute_ssh_command "${SCRIPT1}"
+    sleep 5
+    execute_ssh_command "${SCRIPT2}"
+
+    echo -e "Adding route..."
+    echo "$(date): Ping to ${WAN_IP} failed" >> "${LOG_FILE}"
 else
-    echo "Ping to $wanIp was successful."
+    echo "Ping to ${WAN_IP} was successful."
 fi
